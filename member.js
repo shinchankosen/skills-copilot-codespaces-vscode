106811310472466064332038f367e82c8c0d3fea
shinchan <32698751+shinchankosen@users.noreply.github.com>
2025-04-17 10:55:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+    const skills = [
+        {
+        name: 'JavaScript',
+        level: 'Advanced',
+        },
+        {
+        name: 'Python',
+        level: 'Intermediate',
+        },
+        {
+        name: 'Java',
+        level: 'Beginner',
+        },
+    ];
+    
+    return {
+        skills,
+    };
+}
